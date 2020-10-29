8730a3995c2f0c9990a4c186619d069dbc99416b
strontic <65554290+strontic@users.noreply.github.com>
2020-10-28 22:34:15 -0400

    Update CSV Header Handling
    - Updated the handling of CSV headers so it properly formatted output, and trimmed the sizes where needed. This resolved a bug where certain columns were not being included in the final CSV output even though they existed in the JSON output.

--- script/Coalesce-Json.ps1
+++ script/Coalesce-Json.ps1
@@ -29,61 +29,63 @@ function Coalesce-Json {
             Exit
         }
     }
+    
+    #Create directory for saving output
+    New-Item -ItemType Directory -Force -Path "$save_path" | Out-Null
+
+    # Define CSV columns/headers (Used in Select-Object statement)
+    # Note: If the header name is not defined here, or it does not match the property names defined in the source JSON file, then it will no be included.
+    $csv_props = @(
+        @{ Name = 'file_name'                ; Expression = { $_.file_name } }
+        @{ Name = 'file_path'                ; Expression = { $_.file_path } }
+        @{ Name = 'hash_md5'                 ; Expression = { $_.hash_md5 } }
+        @{ Name = 'hash_sha1'                ; Expression = { $_.hash_sha1 } }
+        @{ Name = 'hash_sha256'              ; Expression = { $_.hash_sha256 } }
+        @{ Name = 'hash_sha384'              ; Expression = { $_.hash_sha384 } }
+        @{ Name = 'hash_sha512'              ; Expression = { $_.hash_sha512 } }
+        @{ Name = 'hash_ssdeep'              ; Expression = { $_.hash_ssdeep } }
+        @{ Name = 'hash_imp'                 ; Expression = { $_.hash_imp } }
+        @{ Name = 'hash_pesha1'              ; Expression = { $_.hash_pesha1 } }
+        @{ Name = 'hash_pe256'               ; Expression = { $_.hash_pe256 } }
+        @{ Name = 'signature_status'         ; Expression = { $_.signature_status } }
+        @{ Name = 'signature_status_message' ; Expression = { $_.signature_status_message } }
+        @{ Name = 'signature_serial'         ; Expression = { $_.signature_serial } }
+        @{ Name = 'signature_thumbprint'     ; Expression = { $_.signature_thumbprint } }
+        @{ Name = 'signature_issuer'         ; Expression = { $_.signature_issuer } }
+        @{ Name = 'signature_subject'        ; Expression = { $_.signature_subject } }
+        @{ Name = 'meta_description'         ; Expression = { $_.meta_description } }
+        @{ Name = 'meta_original_filename'   ; Expression = { $_.meta_original_filename } }
+        @{ Name = 'meta_product_name'        ; Expression = { $_.meta_product_name } }
+        @{ Name = 'meta_comments'            ; Expression = { $_.meta_comments } }
+        @{ Name = 'meta_company_name'        ; Expression = { $_.meta_company_name } }
+        #@{ Name = 'meta_file_name'          ; Expression = { $_.meta_file_name } }
+        @{ Name = 'meta_file_version'        ; Expression = { $_.meta_file_version } }
+        @{ Name = 'meta_product_version'     ; Expression = { $_.meta_product_version } }
+        #@{ Name = 'meta_isdebug'            ; Expression = { $_.meta_isdebug } }
+        #@{ Name = 'meta_ispatched'          ; Expression = { $_.meta_ispatched } }
+        #@{ Name = 'meta_isprerelease'       ; Expression = { $_.meta_isprerelease } }
+        #@{ Name = 'meta_isprivatebuild'     ; Expression = { $_.meta_isprivatebuild } }
+        #@{ Name = 'meta_isspecialbuild'     ; Expression = { $_.meta_isspecialbuild } }
+        @{ Name = 'meta_language'            ; Expression = { $_.meta_language } }
+        @{ Name = 'meta_legal_copyright'     ; Expression = { $_.meta_legal_copyright } }
+        @{ Name = 'meta_legal_trademarks'    ; Expression = { $_.meta_legal_trademarks } }
+        #@{ Name = 'meta_private_build'      ; Expression = { $_.meta_private_build } }
+        #@{ Name = 'meta_special_build'      ; Expression = { $_.meta_special_build } }
+        #@{ Name = 'meta_file_version_raw'   ; Expression = { $_.meta_file_version_raw } }
+        #@{ Name = 'meta_product_version_raw'; Expression = { $_.meta_product_version_raw } }
+        @{ Name = 'meta_machinetype'         ; Expression = { $_.meta_machinetype } }
+        @{ Name = 'output'                   ; Expression = { ($_.output).Substring(0,[math]::min(($_.output).Length,32000)) } } #limit to 32k characters due to cell overflow
+        @{ Name = 'error'                    ; Expression = { ($_.error).Substring(0,[math]::min(($_.output).Length,32000)) } }   #limit to 32k characters due to cell overflow
+        @{ Name = 'children'                 ; Expression = { $_.children } }
+        @{ Name = 'runtime_window_title'     ; Expression = { $_.runtime_window_title } }
+        @{ Name = 'filescan_vtdetection'     ; Expression = { $_.filescan_vtdetection } }
+        @{ Name = 'filescan_vtlink'          ; Expression = { $_.filescan_vtlink } }
+    )
 
     # Get Date
     $time = Get-Date -Format "yyyy-MM-ddTHH-mm-ss"
 
-    # Define the header names. This is only used for export to CSV file -- not JSON. 
-    # If the header name is not defined here, or it does not match the property names defined in the source JSON file, then it will no be included.
-    $header = [PSCustomObject]@{
-        file_name = $null
-        file_path = $null
-        hash_md5 = $null
-        hash_sha1 = $null
-        hash_sha256 = $null
-        hash_sha384 = $null
-        hash_sha512 = $null
-        hash_ssdeep = $null
-        hash_imp    = $null
-        hash_pesha1 = $null
-        hash_pe256  = $null
-        signature_status = $null
-        signature_status_message = $null
-        signature_serial = $null
-        signature_thumbprint = $null
-        signature_issuer = $null
-        signature_subject = $null
-        meta_description = $null
-        meta_original_filename = $null
-        meta_product_name = $null
-        meta_comments = $null
-        meta_company_name = $null
-        #meta_file_name = $null
-        meta_file_version = $null
-        meta_product_version = $null
-        #meta_isdebug = $null
-        #meta_ispatched = $null
-        #meta_isprerelease = $null
-        #meta_isprivatebuild = $null
-        #meta_isspecialbuild = $null
-        meta_language = $null
-        meta_legal_copyright = $null
-        meta_legal_trademarks = $null
-        #meta_private_build = $null
-        #meta_special_build = $null
-        #meta_file_version_raw = $null
-        #meta_product_version_raw = $null
-        meta_machinetype = $null
-		output = $null
-		error = $null
-		children = $null
-        runtime_window_title = $null
-        filescan_vtdetection = $null
-        filescan_vtlink = $null
-    }
-
     $json_obj_group = [PSCustomObject]@{}
-    $json_obj_group | Add-Member  -NotePropertyName header -NotePropertyValue $header
 
     Write-Host "Reading JSON files..."
 
@@ -101,7 +103,7 @@ function Coalesce-Json {
             $json_item_name = $json_item.Name
             $json_item_value = $json_item.Value
 
-            try { $json_obj_group | Add-Member -NotePropertyName $json_item_name -NotePropertyValue $json_item_value -ErrorAction SilentlyContinue } catch { write-host "Failed adding $json_item_name." }
+            try { $json_obj_group | Add-Member -NotePropertyName $json_item_name -NotePropertyValue $json_item_value -ErrorAction Stop } catch { write-host "Failed adding $json_item_name. $json_item_value" }
 
         }
     }
@@ -109,28 +111,30 @@ function Coalesce-Json {
     try {
         Write-Host "Saving coalesced files..."
 
-        if ($save_csv) {
-
-            # Convert output to CSV
-            # Note: The first object in $file_objects defines the column header names.
-            # Note2: This part ".PSObject.Properties | ForEach-Object { $_.value }" is needed for transposing the columns/rows.
-            $csv_output = $json_obj_group.PSObject.Properties | Sort-Object -Property Name | ForEach-Object { $_.value } | ConvertTo-Csv -NoTypeInformation
-
-            #Save to file
-            Write-Host "--> Saving: $save_path\$time-Strontic-xCyclopedia-COMBINED.csv"
-            Set-Content -Path "$save_path\$time-Strontic-xCyclopedia-COMBINED.csv" -Value $csv_output -Encoding UTF8
-
-        }
-
         if ($save_json) {
 
             # Convert output to JSON
-            $json_obj_group.PSObject.Properties.Remove('header') #removes column headers which is only needed for the CSV file
-            $json_output = $json_obj_group | ConvertTo-Json -Depth 4 | Convert-UnicodeToUTF8
+            $json_output = $json_obj_group | ConvertTo-Json -Depth 4 -ErrorAction Stop | Convert-UnicodeToUTF8
 
             #Save to file
             Write-Host "--> Saving: $save_path\$time-Strontic-xCyclopedia-COMBINED.json"
-            Set-Content -Path "$save_path\$time-Strontic-xCyclopedia-COMBINED.json" -Value $json_output -Encoding UTF8
+            Set-Content -Path "$save_path\$time-Strontic-xCyclopedia-COMBINED.json" -Value $json_output -Encoding UTF8 -ErrorAction Stop
+
+        }
+
+        if ($save_csv) {
+
+            #Remove comment property
+            $json_obj_group.PSObject.Properties.Remove('comment') #removes comment property which is only used for the JSON file
+        
+            # Convert output to CSV
+            # Note: The first object in $json_obj_group defines the column header names.
+            # Note2: This part ".PSObject.Properties | ForEach-Object { $_.value }" is needed for transposing the columns/rows.
+            $csv_output = $json_obj_group.PSObject.Properties | Sort-Object -Property Name | ForEach-Object { $_.value } | Select-Object -Property $csv_props | ConvertTo-Csv -NoTypeInformation -ErrorAction Stop
+
+            #Save to file
+            Write-Host "--> Saving: $save_path\$time-Strontic-xCyclopedia-COMBINED.csv"
+            Set-Content -Path "$save_path\$time-Strontic-xCyclopedia-COMBINED.csv" -Value $csv_output -Encoding UTF8 -ErrorAction Stop
 
         }
 
